b98d915c5731fea1788e7edff2cc52212387d8c9
Muxammadali <sherikjumayev1202@gmail.com>
2023-01-30 18:08:54 +0500

    feat: add program

--- program/Program.cs
+++ program/Program.cs
@@ -3,3 +3,32 @@
 // длина которых меньше либо равна 3 символа. Первоначальный массив можно ввести с клавиатуры, 
 // либо задать на старте выполнения алгоритма. При решении не рекомендуется пользоваться коллекциями, 
 // лучше обойтись исключительно массивами.
+Console.WriteLine("Введите слова");
+string input = Console.ReadLine();
+string[] words = input.Split(" ");
+
+string[] ArraySort(string[] array)
+{
+    string[] NewArray = new string[array.Length];
+    int x = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i].Length <= 3)
+        {
+            NewArray[x] = array[i];
+            x++;
+        }
+    }
+    return NewArray;
+}
+
+void PrintArray(string[] arr)
+{
+    for (int i = 0; i < arr.Length; i++)
+    {
+        Console.Write(arr[i] + " ");
+    }
+}
+
+string[] NewWords = ArraySort(words);
+PrintArray(NewWords);
